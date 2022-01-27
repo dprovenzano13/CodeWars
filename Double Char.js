25c703236686be8d12be10a5e6bc55c53f236c63
David Provenzano <dprovenzano13@gmail.com>
2022-01-27 09:09:46 -0500

    Add final solution

--- Double Char.js
+++ Double Char.js
@@ -6,10 +6,12 @@ Examples (Input -> Output):
 * "1234!_ "     -> "11223344!!__  "
 Good Luck!*/
 
-function double(str) {
-    let doubledString = ''
-for(let i = 0; i < str.length; i++){
-  doubledString += str[i] + str[i]
-}
-return doubledString
-}
+// function double(str) {
+//     let doubledString = ''
+// for(let i = 0; i < str.length; i++){
+//   doubledString += str[i] + str[i]
+// }
+// return doubledString
+// }
+
+const doubleChar = (str) => str.split("").map(c => c + c).join("");
