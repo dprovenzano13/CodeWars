1b32e1812fb0a00c4c2f4652ee3257479558d642
David Provenzano <dprovenzano13@gmail.com>
2022-01-31 19:35:31 -0500

    Add final solution

--- 7th Kyu/Sum of the first nth term of Series.js
+++ 7th Kyu/Sum of the first nth term of Series.js
@@ -15,3 +15,12 @@ Examples:(Input --> Output)
 2 --> 1 + 1/4 --> "1.25"
 5 --> 1 + 1/4 + 1/7 + 1/10 + 1/13 --> "1.57"*/
 
+function SeriesSum(n) {
+    let sum = 0
+    let baseDom = 1
+    for(let i = 0; i < n; i++){
+      let mod = i * 3
+      sum += 1/ (baseDom + mod)
+    }
+    return String(sum.toFixed(2))
+  }
