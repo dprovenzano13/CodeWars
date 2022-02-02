ff343d71daa712802306d026b57e2a82ac07a03f
David Provenzano <dprovenzano13@gmail.com>
2022-02-02 11:11:34 -0500

    Add first solution

--- 8th Kyu/Rock Paper Scissors.js
+++ 8th Kyu/Rock Paper Scissors.js
@@ -7,3 +7,13 @@ Examples(Input1, Input2 --> Output):
 "scissors", "paper" --> "Player 1 won!"
 "scissors", "rock" --> "Player 2 won!"
 "paper", "paper" --> "Draw!" */
+
+function rockPaperScissors(input1, input2) {
+    if(input1 === 'rock' && input2 === 'scissors' || input1 === 'paper' && input2 === 'rock' || input1 === 'scissors' && input2 === 'paper'){
+        return 'Player 1 won!'
+    } else if (input1 === 'scissors' && input2 === 'rock' || input1 === 'rock' && input2 === 'paper' || input1 === 'paper' && input2 === 'scissors'){
+        return 'Player 2 won!'
+    } else if (input1 === input2){
+        return 'Draw!'
+    }
+}
