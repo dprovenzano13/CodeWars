81e100f74aab85bd32f9cedd122237fee1566444
David Provenzano <dprovenzano13@gmail.com>
2022-02-10 10:07:50 -0500

    Add first solution

--- 8th Kyu/Sentence Smash.js
+++ 8th Kyu/Sentence Smash.js
@@ -5,3 +5,8 @@ Write a function that takes an array of words and smashes them together into a s
 Example
 ['hello', 'world', 'this', 'is', 'great']  =>  'hello world this is great'
 */
+
+function smash (words) {
+    let sentence = words.join(' ')
+    return sentence
+ };
