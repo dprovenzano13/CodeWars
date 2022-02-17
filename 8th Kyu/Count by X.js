1c58a19d4665ff5cd06c23ed107c928d83e4f917
David Provenzano <dprovenzano13@gmail.com>
2022-02-16 23:13:33 -0500

    Add final solution

--- 8th Kyu/Count by X.js
+++ 8th Kyu/Count by X.js
@@ -9,12 +9,10 @@ Examples:
 countBy(1,10) === [1,2,3,4,5,6,7,8,9,10]
 countBy(2,5) === [2,4,6,8,10] */
 
-  function countBy(x, n){
-    let z = [];
-    let startNum = x
-    for(let i = 0; i < n; i++){
-      z.push(x)
-      x += startNum
-    }
-    return z;
+function countBy(x, n) {
+  var z = [];
+  for (i = 1; i <= n; i++) {
+      z.push(x * i);
   }
+  return z;
+}
