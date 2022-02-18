22f402f4c435adeca29d85b63d27ce6ae86682b2
David Provenzano <dprovenzano13@gmail.com>
2022-02-18 10:48:41 -0500

    Add final solution

--- Sum Arrays.js
+++ Sum Arrays.js
@@ -19,6 +19,4 @@ We're testing basic loops and math operations. This is for beginners who are jus
 Advanced users may find this extremely easy and can easily write this in one line.
 */
 
-function sum (numbers) {
-    return numbers.reduce((acc, c) => acc + c, 0)
-};
+let sum = sum.reduce((acc, c) => acc + c, 0)
