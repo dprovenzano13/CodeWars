b1e8edf9bb4719507f5782acc1444dcbdce85257
David Provenzano <dprovenzano13@gmail.com>
2022-02-24 18:50:57 -0500

    Add final solution

--- 8th Kyu/Multiply Two Nums.js
+++ 8th Kyu/Multiply Two Nums.js
@@ -1,6 +1,6 @@
 //Function 3 - multiplying two numbers
 //Implement a function which multiplies two numbers.
 
-const multiply = (num1, num2) => {
-    return num1 * num2
+const multiply = (a, b) => {
+    return a * b
 }
