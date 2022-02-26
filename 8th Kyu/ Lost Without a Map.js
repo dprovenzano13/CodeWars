6bfc9cda68ce68fca4cbfb951dd4c67be77befc8
David Provenzano <dprovenzano13@gmail.com>
2022-02-25 19:01:11 -0500

    Add square sum challenge

---
+++ 8th Kyu/ Lost Without a Map.js
@@ -0,0 +1,16 @@
+//Given an array of integers, return a new array with each value doubled.
+
+//For example:
+
+//[1, 2, 3] --> [2, 4, 6]
+
+// Make a function that takes in an array and doubles the elements inside of the array
+function doubled(arr){
+   return arr.map(e => e * 2)
+}
+// return a new array with each value doubled
+
+doubled([1,3,4]);
+=======
+//[1, 2, 3] --> [2, 4, 6]
+
