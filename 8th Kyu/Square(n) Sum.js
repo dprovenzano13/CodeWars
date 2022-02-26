4524c78048135b78e9499bb6be96103b4efd1bff
David Provenzano <dprovenzano13@gmail.com>
2022-02-26 11:28:35 -0500

    Solution before test

--- 8th Kyu/Square(n) Sum.js
+++ 8th Kyu/Square(n) Sum.js
@@ -1,7 +1,15 @@
 //Complete the square sum function so that it squares each number passed into it and then sums the results together.
 
 //For example, for [1, 2, 2] it should return 9 because 1^2 + 2^2 + 2^2 = 9.
+let nums = [1, 2, 2]
 
-function squareSum(numbers){
- console.log(numbers**)
+function squareSum(arr){
+  let sum = 0
+    arr.forEach(e =>{
+    let square = Math.pow(e,2)
+    return sum += square
+    })
+  return sum
 }
+
+squareSum(nums)
