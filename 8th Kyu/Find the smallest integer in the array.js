263a5f4a14ed5144056d56cdd9a0c9a53e052f97
David Provenzano <dprovenzano13@gmail.com>
2022-06-25 16:18:52 -0400

    Add first solution

--- 8th Kyu/Find the smallest integer in the array.js
+++ 8th Kyu/Find the smallest integer in the array.js
@@ -6,3 +6,5 @@ For example:
 Given [34, 15, 88, 2] your solution will return 2
 Given [34, -345, -1, 100] your solution will return -345
 You can assume, for the purpose of this kata, that the supplied array will not be empty.*/
+
+const min = (list) => Math.min(...list);
