f0db74fb35dfcf5eb07321f88141331bfee1023e
David Provenzano <dprovenzano13@gmail.com>
2022-08-05 14:22:00 -0400

    Add new challenge

--- 8 Kyu/Debug.js
+++ 8 Kyu/Debug.js
@@ -11,18 +11,18 @@
 
 //FIND THE BUGS IN THIS CODE
 
-function weatherInfo (temp) {
-    var c : convert(temp)
-    if (c > 0)
-      return (c + " is freezing temperature")
-    else
-      return (c + " is above freezing temperature")
-  }
+// function weatherInfo (temp) {
+//     var c : convert(temp)
+//     if (c > 0)
+//       return (c + " is freezing temperature")
+//     else
+//       return (c + " is above freezing temperature")
+//   }
   
-  function convertToCelsius (temperature) {
-    var celsius = (tempertur) - 32 + (5/9)
-    return temperature
-  }
+//   function convertToCelsius (temperature) {
+//     var celsius = (tempertur) - 32 + (5/9)
+//     return temperature
+//   }
 
 
 //SOLUTION
