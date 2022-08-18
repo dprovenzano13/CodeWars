c75183c814effea353a1d44d5a45c281635e7e8e
David Provenzano <dprovenzano13@gmail.com>
2022-08-18 07:54:37 -0400

    Add final solution

--- 6 Kyu/Replace With Alphabet Position.js
+++ 6 Kyu/Replace With Alphabet Position.js
@@ -10,3 +10,14 @@
 // alphabetPosition("The sunset sets at twelve o' clock.")
 // Should return "20 8 5 19 21 14 19 5 20 19 5 20 19 1 20 20 23 5 12 22 5 15 3 12 15 3 11" ( as a string )
 
+function alphabetPosition(text) {
+    const arr = []
+    const textLower = text.toLowerCase()
+    for(let i = 0; i < textLower.length; i++){
+      let code = textLower.charCodeAt(i)
+      if(code >= 97 && code <= 122) {
+        arr.push(code - 96)
+      }
+    }
+        return arr.join(' ')
+    }
