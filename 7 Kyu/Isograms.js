f3bc8adfb5e2acba6cd49174354d4acb9704274a
David Provenzano <dprovenzano13@gmail.com>
2022-08-20 12:41:11 -0400

    Add first solution

--- 7 Kyu/Isograms.js
+++ 7 Kyu/Isograms.js
@@ -1,7 +1,24 @@
-An isogram is a word that has no repeating letters, consecutive or non-consecutive. Implement a function that determines whether a string that contains only letters is an isogram. Assume the empty string is an isogram. Ignore letter case.
+// An isogram is a word that has no repeating letters, consecutive or non-consecutive. Implement a function that determines whether a string that contains only letters is an isogram. Assume the empty string is an isogram. Ignore letter case.
 
-Example: (Input --> Output)
+// Example: (Input --> Output)
 
-"Dermatoglyphics" --> true
-"aba" --> false
-"moOse" --> false (ignore letter case)
+// "Dermatoglyphics" --> true
+// "aba" --> false
+// "moOse" --> false (ignore letter case)
+
+function isIsogram(str){
+	let lower = str.toLowerCase().split('')
+  let newArr = []
+  lower.forEach(e => {
+    if(! newArr.includes(e)){
+ 			newArr.push(e)
+    }
+  })
+    if(String(lower) === ''){
+    return true
+  } else if(String(newArr) === String(lower)){
+    return true
+  } else {
+    return false
+  }
+}
