eecc8b2cd33b5784e7e2f2ea4bffbe15caa4831a
David Provenzano <dprovenzano13@gmail.com>
2022-08-26 10:38:32 -0400

    Add function

--- 7 Kyu/Disemvowel Trolls.js
+++ 7 Kyu/Disemvowel Trolls.js
@@ -7,3 +7,7 @@
 // For example, the string "This website is for losers LOL!" would become "Ths wbst s fr lsrs LL!".
 
 // Note: for this kata y isn't considered a vowel.
+
+function disemvowel(str) {
+    return str;
+  }
