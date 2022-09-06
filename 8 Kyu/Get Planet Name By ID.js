f6a38a1ae598c9d32828a10d043e7a66b6119a47
David Provenzano <dprovenzano13@gmail.com>
2022-09-06 14:33:08 -0400

    Add final solution

--- 8 Kyu/Get Planet Name By ID.js
+++ 8 Kyu/Get Planet Name By ID.js
@@ -4,25 +4,61 @@
 
 // 3 --> "Earth"
 
+// function getPlanetName(id){
+//     var name;
+//     switch(id){
+//       case 1:
+//         name = 'Mercury'
+//       case 2:
+//         name = 'Venus'
+//       case 3:
+//         name = 'Earth'
+//       case 4:
+//         name = 'Mars'
+//       case 5:
+//         name = 'Jupiter'
+//       case 6:
+//         name = 'Saturn'
+//       case 7:
+//         name = 'Uranus'
+//       case 8:
+//         name = 'Neptune'
+//     }
+    
+//     return name;
+//   }
+// getPlanetName(2), 'Venus'
+// getPlanetName(5), 'Jupiter'
+// getPlanetName(3), 'Earth'
+
+
 function getPlanetName(id){
     var name;
     switch(id){
       case 1:
         name = 'Mercury'
+          break;
       case 2:
         name = 'Venus'
+          break;
       case 3:
         name = 'Earth'
+          break;
       case 4:
         name = 'Mars'
+          break;
       case 5:
         name = 'Jupiter'
+          break;
       case 6:
         name = 'Saturn'
+          break;
       case 7:
         name = 'Uranus'
+          break;
       case 8:
         name = 'Neptune'
+          break;
     }
     
     return name;
