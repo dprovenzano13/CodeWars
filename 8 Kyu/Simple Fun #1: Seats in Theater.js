71b2c88d03b0b8239b289313008fb2d0f93d6e83
David Provenzano <dprovenzano13@gmail.com>
2022-09-11 10:23:57 -0400

    Add first solution

--- 8 Kyu/Simple Fun #1: Seats in Theater.js
+++ 8 Kyu/Simple Fun #1: Seats in Theater.js
@@ -40,3 +40,7 @@
 
 // [output] an integer
 // The number of people who sit strictly behind you and in your column or to the left.
+
+function seatsInTheater(nCols, nRows, col, row) {
+    return (nCols - col + 1) * (nRows - row)
+    }
