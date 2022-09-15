f0207a35e7f9a3091b19113cd72583e81312cc1f
David Provenzano <dprovenzano13@gmail.com>
2022-09-15 13:32:42 -0400

    Add final solution

--- 8 Kyu/Remove duplicates from list.js
+++ 8 Kyu/Remove duplicates from list.js
@@ -1,3 +1,13 @@
 // Define a function that removes duplicates from an array of numbers and returns it as a result.
 
 // The order of the sequence has to stay the same.
+
+function distinct(a) {
+    let doup = [];
+    for(let i = 0; i < a.length; i++){
+      if(! doup.includes(a[i])){
+        doup.push(a[i])
+      }
+    }
+    return doup
+  }
